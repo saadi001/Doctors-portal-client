3659a0f78200a48d3e539d5095b4c0e5f6840847
Sheikh sadi <you@example.com>
2022-11-14 22:57:45 +0600

    banner added

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import chair from '../../../assets/images/chair.png'
+import './Banner.css'
 
 const Banner = () => {
      return (
-          <div>
-               this is banner
+          <div className="hero hero-css">
+               <div className="hero-content  flex-col lg:flex-row-reverse">
+                    <img src={chair} alt='' className="rounded-lg w-1/2 shadow-2xl" />
+                    <div>
+                         <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                         <button className="btn btn-primary text-white bg-gradient-to-r from-primary to-secondary">Get Started</button>
+                    </div>
+               </div>
           </div>
      );
 };
